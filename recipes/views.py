6cdde12f92003f465198d39eaffd3325c95e4c58
flavio <flavio@tins.com.br>
2024-06-01 23:32:11 -0300

    correrções e implementações do Django Templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', {'name': 'Flávio Moura'})
+    return render(request, 'recipes/pages/home.html', {'recipes': [make_recipe() for _ in range(10)]})
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', {'name': 'Flávio Moura'})
+    return render(request, 'recipes/pages/recipe-view.html', {
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
