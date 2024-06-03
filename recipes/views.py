54eb850c68a32a3e37d5434e9024e44570f6d68f
flavio <flavio@tins.com.br>
2024-06-02 23:22:02 -0300

    Restante da implementação dos Django models e view e apresentação de detalhes e home dos templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,21 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 
 # Create your views here.
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', {'recipes': recipes})
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', {
         'recipe': recipe,
         'is_detail_page': True,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', {'recipes': recipes})
+    recipes = get_list_or_404(
+        Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+    return render(request, 'recipes/pages/category.html', {'recipes': recipes, 'title': f'{recipes[0].category.name} - Category'})
